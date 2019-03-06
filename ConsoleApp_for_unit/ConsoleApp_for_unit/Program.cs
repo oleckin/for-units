4cb6d71c8ed66bdbc7298abd4803439d852d75c4
Olesya Simakova <o.simakova@dodopizza.com>
2019-03-06 16:11:09 +0300

    add ConsoleApp_for_unit

---
+++ ConsoleApp_for_unit/ConsoleApp_for_unit/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp_for_unit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Коля, как дела? Нажми любую кнопку, чтобы закончить меня.");
+            //Console.ReadLine();
+            Console.WriteLine("Введи два числа, чтобы продолжить игру.");
+
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            
+            int num1 = Int32.Parse(a);
+            int num2 = Int32.Parse(b);
+
+            int result = num1 + num2;
+            Console.WriteLine(result);
+            
+            result = Int32.MaxValue;
+            Console.WriteLine(result);
+
+            result = num1 * num2;
+            Console.WriteLine(result);
+
+            result = (num1 * num2 / 2);
+            Console.WriteLine(result);
+
+            double average = (double) (num1 + num2) / 2;
+            Console.WriteLine(average);
+
+        }
+    }
+}
