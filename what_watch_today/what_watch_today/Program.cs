8c839c9910bdd15eeda1d2935d4830e462b9592d
Olesya Simakova <o.simakova@dodopizza.com>
2019-03-06 16:08:05 +0300

    add new dir

---
+++ what_watch_today/what_watch_today/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace what_watch_today
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как тебя зовут?");
+            String name = Console.ReadLine();
+            String text = ("Привет," + name + " Я помогу выбрать тебе мультик на вечер!");
+            Console.WriteLine("Привет, " + name + "! Я помогу выбрать тебе мультик на вечер!");
+            
+            Console.WriteLine("Какой мультик ты хочешь посмотреть весёлый, добрый, музыкальный, поучительный или сказку?");
+            
+            String genre = Console.ReadLine();
+            
+            if (genre == "весёлый")
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Тогда посоветую вот такие:");
+                Console.WriteLine("Ну, погоди!");
+                Console.WriteLine("Простоквашино.");
+                Console.WriteLine("Бобик в гостях у Барбоса.");
+            }
+            
+            else if (genre == "добрый")
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("Тогда посоветую вот такие:");
+                Console.WriteLine("Девочка и спички"); 
+                Console.WriteLine("Дюймовочка");
+            }
+            
+            else if (genre == "музыкальный")
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Тогда посоветую вот такие:");
+                Console.WriteLine("Бременские музыканты.");
+                Console.WriteLine("Летучий корабль.");
+            } 
+            
+            else if (genre == "поучительный")
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Тогда посоветую вот такие:");
+                Console.WriteLine("Тётушка Сова.");
+                Console.WriteLine("Фиксики.");
+                Console.WriteLine("Смешарики. Пин-код.");
+            }
+           
+            else         
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Аленький цветочек.");
+                Console.WriteLine("Гора самоцветов.");
+                Console.WriteLine("Конёк-горбунок.");    
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
